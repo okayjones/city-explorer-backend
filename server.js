271fe5289d5b67102a7f5b6dea8e2dcfc23b3fbd
Logan Jones <loganemilyjones@mac.com>
2020-10-15 17:30:14 -0700

    Formatting cleanup

--- server.js
+++ server.js
@@ -9,34 +9,21 @@ app.use(cors());
 
 
 // Routes
-app.get('/', (request, response) => {
-    console.log('Hello world, this is the home route!');
-});
-
 app.get('/location', (request, response) => {
     const data = require('./data/location.json')[0];
     const city = request.query.city;
-
     const location = new Location(data, city);
-
     response.send(location);
 });
 
 app.get('/weather', (request, response) => {
     const data = require('./data/weather.json').data;
-    const city = request.query.city;
-
     let weather = [];
-    
-    data.forEach(data => {
-        const day = new Weather(data);
-        weather.push(day);
-    });
-
+    data.forEach(data => weather.push(Weather(data)));
     response.send(weather);
 });
 
-
+// Constructors
 function Location(obj, query){ //location constructor
     this.search_query = query;
     this.formatted_query = obj.display_name;
@@ -49,6 +36,7 @@ function Weather(obj){ //weather constructor
     this.time = new Date(obj.valid_date).toDateString();
 }
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server now listening on port ${PORT}`);
 });
